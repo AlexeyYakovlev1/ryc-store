3fa08d3ad776d051f17b503d3a31c410dad018f1
Alexey Yakovlev <alexeyykovlev555@gmail.com>
2020-08-30 18:23:04 +0300

    загрузил проект

---
+++ src/scripts/script.js
@@ -0,0 +1,115 @@
+const listCards = document.querySelector('.list-cards');
+const headerSearchWord = document.querySelector('.header-search-word');
+const headerSearch = document.querySelector('.header-search');
+
+function checkResponse(res) {
+    if (res.length) {
+        document.querySelector('.no-el').style.display = 'none';
+    } else {
+        document.querySelector('.no-el').style.display = 'block';
+    }
+}
+
+// создаем карточки
+const createProducts = response => {
+    listCards.innerHTML = '';
+
+    response.forEach(product => {
+        let oldPrice;
+        let sale;
+
+        if (product.sale <= 0) {
+            oldPrice = '';
+            sale = '';
+        } else {
+            sale = `-(${product.sale}%)`;
+            oldPrice = `${product.oldPrice} Руб.`;
+        }
+
+        let card = `
+            <li class="card">
+                <a href="#" title="${product.name}">
+                    <div class="card__block-img">
+                        <img src="${product.img}" alt="${product.name}">
+                    </div>
+                    <div class="card__description">
+                        <span class="card-name">${product.name}</span>
+                        <div class="card__block-price">
+                            <span class="card-old-price">${oldPrice}</span>
+                            <span class="card-now-price">${product.nowPrice} Руб.</span>
+                            <span class="card-sale">${sale}</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        `
+
+        listCards.innerHTML += card;
+    })
+}
+
+function animationList() {
+    headerSearchWord.addEventListener('click', event => {
+        if (event.target) {
+            document.querySelector('.header__bottom-list-user').style.width = '315px';
+            document.querySelector('.header__block-search').style.width = '170px';
+            document.querySelector('.header__block-search-content').style.transform = 'translateX(-38px)';
+
+            headerSearch.focus();
+        }
+    });
+
+    headerSearch.addEventListener('blur', () => {
+        document.querySelector('.header__bottom-list-user').style.width = '200px';
+        document.querySelector('.header__block-search').style.width = '36px';
+        document.querySelector('.header__block-search-content').style.transform = 'translateX(0)';
+    });
+}
+
+animationList();
+
+window.addEventListener('DOMContentLoaded', () => {
+    // функция для запросов
+    const getResource = async(url) => {
+        // дожидаемся операции подключения
+        const res = await fetch(`${url}`);
+
+        // проверка на успешный запрос
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        // дожидаемся пока код завершит операцию превращения json в обычный объект
+        return await res.json();
+    }
+
+    const req = () => {
+        // запрос
+        getResource('http://localhost:3000/products')
+            // обработка данных, которые приходят по этому запросу
+            .then(data => {
+                createProducts(data);
+                search(data);
+            })
+            // ошибка
+            .catch(err => console.log(err));
+    }
+
+    req();
+});
+
+// поиск
+const search = (data) => {
+    headerSearch.addEventListener('input', () => {
+        const valueSearch = headerSearch.value.trim().toLowerCase();
+        const result = data.filter(item => item.name.toLowerCase().includes(valueSearch));
+
+        if (valueSearch.length > 2) {
+            createProducts(result);
+            checkResponse(result)
+        } else {
+            createProducts(result);
+            checkResponse(result)
+        }
+    });
+}
---
+++ src/scripts/slider.js
@@ -0,0 +1,8 @@
+import 'slick-carousel';
+import $ from 'jquery';
+
+$('.title__slider').slick({
+    arrows: false,
+    dots: false,
+    autoplay: 4000
+});
